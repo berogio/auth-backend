e61e5230dd274c08b921bd550249d4631de8d45a
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-03-02 13:42:57 +0100

    adgadgadg

--- server.js
+++ server.js
@@ -2,23 +2,18 @@ import express from 'express';
 import cors from 'cors'
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
 app.use(cors());
 app.use(bodyParser.json());
 
 //mongose
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
 const url =
-    `mongodb+srv://gberi2012:${password}@cluster0.a2bfzeu.mongodb.net/test`
+    `mongodb+srv://gberi2012:${process.env.DB_PASSWORD}@cluster0.a2bfzeu.mongodb.net/test`
 mongoose.set('strictQuery', false);
 
 const noteSchema = new mongoose.Schema({
@@ -53,7 +48,7 @@ app.post('/login', (req, res, next) => {
     User.find({ Email: email, Password: password }, (err, data) => {
         if (data.length > 0) {
             res.sendStatus(200)
-        } else res.sendStatus(404)
+        } else res.sendStatus(401)
     })
 })
 
